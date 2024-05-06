a0693b17a5b4ecd97bdac3dfdcc2dc30c5803eb7
Po-Wei Wang (Vincent) <poweiw@nvidia.com>
2024-05-06 10:51:24 -0700

    set default shell to bash

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<A-o>', '<cmd> ClangdSwitchSourceHeader <CR>', {})
 vim.api.nvim_set_keymap('n', '<Leader>d', '<cmd> Bdelete <CR>', {})
+vim.api.nvim_set_option('shell', '/bin/bash')
