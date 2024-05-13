e131096762bd20929718c6853cedc9dca6d459bb
Po-Wei Wang (Vincent) <poweiw@nvidia.com>
2024-05-13 16:25:15 -0700

    added diffview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -54,6 +54,39 @@ local plugins = {
     lazy = false,
     branch = "v0.5.8",
     event = "User AstroFile",
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "TimUntersberger/neogit",
+        config = {
+          disable_commit_confirmation = true
+        }
+      },
+    },
+    commit = "9359f7b1dd3cb9fb1e020f57a91f8547be3558c6",
+    keys = {
+      {
+        "<C-g>",
+        "<CMD>DiffviewOpen<CR>",
+        mode = { "n", "i", "v"
+        }
+      }
+    },
+    config = {
+      keymaps = {
+        view = {
+          ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+          ["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+        },
+        file_panel = {
+          ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+          ["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+        },
+      },
+    }
   }
 }
 return plugins
